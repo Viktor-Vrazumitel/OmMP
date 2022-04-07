598a876923e50bcf6b8e56c852290ee863a9ee02
Viktor-Vrazumitel <Viktor_bogatyrev@mail.ru>
2022-04-07 15:47:51 +0300

    baseCgeate

---
+++ server/db/models/room.js
@@ -0,0 +1,27 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Room extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User }) {
+      this.hasMany(User, {
+        foreignKey: "user_id",
+      });
+    }
+  }
+  Room.init(
+    {
+      title: DataTypes.STRING,
+      user_id: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Room",
+    }
+  );
+  return Room;
+};
