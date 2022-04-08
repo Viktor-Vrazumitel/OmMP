6e6b29e88e13c3f3c9b8454fad3210a092841835
rKefear <sl.malyutin@gmail.com>
2022-04-08 14:17:03 +0300

    fix2

---
+++ client/src/components/Room/Room.jsx
@@ -0,0 +1,16 @@
+
+
+function Room() {
+  return ( 
+    <>
+    <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Название комнаты</h5>
+        <h6 className="card-subtitle mb-2 text-muted">Теги</h6>
+      </div>
+    </div>
+    </>
+   );
+}
+
+export default Room;
