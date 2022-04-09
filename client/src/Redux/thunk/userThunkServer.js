35054b7d6e6eff231120a028bde0a56b27273d05
rKefear <sl.malyutin@gmail.com>
2022-04-09 16:59:02 +0300

    fix

--- client/src/Redux/thunk/userThunkServer.js
+++ client/src/Redux/thunk/userThunkServer.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
+import { URL_BASE } from '../../config'
 import { addFriendAction } from '../actions/userAction'
 
 export const findBaseUser = (login) => (dispatch) =>{
-axios.post('http://localhost:3001/search', {login})
+axios.post(`${URL_BASE}/search`, {login})
 .then(res=> dispatch(addFriendAction(res.data)))
 }
 
--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
-const URL_BASE = 'http://192.168.1.133:3001'
+const URL_BASE = 'http://192.168.1.142:3001'
 
 export{URL_BASE}
